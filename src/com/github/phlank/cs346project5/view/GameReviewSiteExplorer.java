47ff8408f1713f12817223347bbee3271fc8a955
Colin Ledbetter <Colin Ledbetter@10.64.67.83>
2016-04-16 20:00:55 -0400

    Added GameReviewSiteExplorer

---
+++ src/com/github/phlank/cs346project5/view/GameReviewSiteExplorer.java
@@ -0,0 +1,18 @@
+package com.github.phlank.cs346project5.view;
+
+import javax.swing.JFrame;
+
+public class GameReviewSiteExplorer extends JFrame {
+	
+	public GameReviewSiteExplorer() {
+		super();
+		this.setVisible(true);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setSize(500, 500);
+	}
+
+	public static void main(String[] args) {
+		GameReviewSiteExplorer siteExplorer = new GameReviewSiteExplorer();
+	}
+
+}
